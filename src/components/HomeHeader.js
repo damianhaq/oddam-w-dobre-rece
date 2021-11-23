19c70b4fabd2e31df4de4fa44709bd106d41f04c
damianhaq <40294540+damianhaq@users.noreply.github.com>
2021-11-23 12:40:39 +0100

    #1b

---
+++ src/components/HomeHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HomeHeader() {
+  return (
+    <div className="HomeHeader">
+      <img src="../assets/Home-Hero-Image.jpg" alt="box" />
+      <div className="test"></div>
+      <p>iksde</p>
+    </div>
+  );
+}
